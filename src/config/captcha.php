59a1081fdf82faefefbe036655f70ec8c656a3cb
DaxitZadafiya <daxitzadafiya.work@gmail.com>
2024-12-11 18:45:22 +0530

    updated config file

--- src/config/captcha.php
+++ src/config/captcha.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'secret' => config('services.recaptcha.secret_key', env('RECAPTCHA_SECRET_KEY')),
-    'sitekey' => config('services.recaptcha.site_key', env('RECAPTCHA_SITE_KEY')),
+    'secret' => config('params.recaptcha_secret_site_key', env('RECAPTCHA_SECRET_KEY')),
+    'sitekey' => config('params.recaptcha_site_key', env('RECAPTCHA_SITE_KEY')),
     'options' => [
         'timeout' => 30,
     ],
